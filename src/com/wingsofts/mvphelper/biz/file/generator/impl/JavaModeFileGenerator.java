9dc6f6aacb5d9de00b424af95d092f5ec37d89cc
mafeibiao <mafeibiao@pashanhoo.com>
2018-09-21 14:15:02 +0800

    add

--- src/com/wingsofts/mvphelper/biz/file/generator/impl/JavaModeFileGenerator.java
+++ src/com/wingsofts/mvphelper/biz/file/generator/impl/JavaModeFileGenerator.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiJavaCodeReferenceElement;
 import com.intellij.psi.PsiMethod;
 
 /**
@@ -31,8 +32,8 @@ public class JavaModeFileGenerator extends BaseFileGenerator {
 
     private void generateView() {
         generateFile(myViewDir, myPrefix + "View", JavaTemplateUtil.INTERNAL_CLASS_TEMPLATE_NAME, (javaFile, psiClass) -> {
-            PsiClass contractClass = myShortNamesCache.getClassesByName(myPrefix + "Contract", myProjectScope)[0];
-            PsiClass view = contractClass.findInnerClassByName("View", false);//don't need to search base
+            PsiClass contractClass = myShortNamesCache.getClassesByName("I" + myPrefix + "Contract", myProjectScope)[0];
+            PsiClass view = contractClass.findInnerClassByName("IView", false);//don't need to search base
             psiClass.getExtendsList().add(myFactory.createReferenceElementByFQClassName("LAMvpView", myProjectScope));
             psiClass.getImplementsList().add(myFactory.createClassReferenceElement(view));
             psiClass.getModifierList().setModifierProperty("public", true);//force 'public interface myPrefixContract'
@@ -40,20 +41,19 @@ public class JavaModeFileGenerator extends BaseFileGenerator {
     }
 
     private void generateContract() {
-        generateFile(myContractDir, myPrefix + "Contract", JavaTemplateUtil.INTERNAL_INTERFACE_TEMPLATE_NAME, (javaFile, psiClass) -> {
-            PsiClass model = myFactory.createInterface("Model");
-            PsiClass view = myFactory.createInterface("View");//You have to achieve 'View' yourself.
-            PsiClass presenter = myFactory.createInterface("Presenter");
+        generateFile(myContractDir, "I" + myPrefix + "Contract", JavaTemplateUtil.INTERNAL_INTERFACE_TEMPLATE_NAME, (javaFile, psiClass) -> {
+            PsiClass model = myFactory.createInterface("IModel");
+            PsiClass view = myFactory.createInterface("IView");//You have to achieve 'View' yourself.
+            PsiClass presenter = myFactory.createInterface("IPresenter");
 
             model.getModifierList().setModifierProperty("public", false);//Remove modifier
             model.getExtendsList().add(myFactory.createReferenceElementByFQClassName("ILAMvpModel", myProjectScope));
             view.getModifierList().setModifierProperty("public", false);
             view.getExtendsList().add(myFactory.createReferenceElementByFQClassName("ILAMvpView", myProjectScope));
             presenter.getModifierList().setModifierProperty("public", false);
-            presenter.getExtendsList().add(myFactory.createReferenceElementByFQClassName("ILAMvpPresenter", myProjectScope));
-            String methodText = buildInitInterfaceMethodText();
-            PsiMethod psiMethod = myFactory.createMethodFromText(methodText, psiClass);
-            presenter.add(psiMethod);
+            PsiJavaCodeReferenceElement ilaMvpPresenter = myFactory.createReferenceElementByFQClassName("ILAMvpPresenter", myProjectScope);
+
+            presenter.getExtendsList().add(ilaMvpPresenter);
 
             psiClass.add(model);
             psiClass.add(view);
@@ -68,11 +68,8 @@ public class JavaModeFileGenerator extends BaseFileGenerator {
 
     private void generatePresenter() {
         generateFile(myPresenterDir, myPrefix + "Presenter", JavaTemplateUtil.INTERNAL_CLASS_TEMPLATE_NAME, (javaFile, psiClass) -> {
-            PsiClass contractClass = myShortNamesCache.getClassesByName(myPrefix + "Contract", myProjectScope)[0];
-            PsiClass presenter = contractClass.findInnerClassByName("Presenter", false);//don't need to search base
-            String methodText = buildInitInterfaceImplMethodText();
-            PsiMethod psiMethod = myFactory.createMethodFromText(methodText, psiClass);
-            psiClass.add(psiMethod);
+            PsiClass contractClass = myShortNamesCache.getClassesByName("I" + myPrefix + "Contract", myProjectScope)[0];
+            PsiClass presenter = contractClass.findInnerClassByName("IPresenter", false);//don't need to search base
 
 
             psiClass.getExtendsList().add(myFactory.createReferenceElementByFQClassName("LAMvpPresenter", myProjectScope));
@@ -82,19 +79,10 @@ public class JavaModeFileGenerator extends BaseFileGenerator {
         });
     }
 
-    private String buildInitInterfaceMethodText() {
-        return "void init();";
-    }
-
-    private String buildInitInterfaceImplMethodText() {
-        return "public void init() {\n" + "\n" +
-                "    }";
-    }
-
     private void generateModel() {
         generateFile(myModelDir, myPrefix + "Model", JavaTemplateUtil.INTERNAL_CLASS_TEMPLATE_NAME, (javaFile, psiClass) -> {
-            PsiClass contractClass = myShortNamesCache.getClassesByName(myPrefix + "Contract", myProjectScope)[0];
-            PsiClass model = contractClass.findInnerClassByName("Model", false);//don't need to search base
+            PsiClass contractClass = myShortNamesCache.getClassesByName("I" + myPrefix + "Contract", myProjectScope)[0];
+            PsiClass model = contractClass.findInnerClassByName("IModel", false);//don't need to search base
             psiClass.getExtendsList().add(myFactory.createReferenceElementByFQClassName("LAMvpModel", myProjectScope));
             psiClass.getImplementsList().add(myFactory.createClassReferenceElement(model));
             psiClass.getModifierList().setModifierProperty("public", true);//force 'public interface myPrefixContract'
